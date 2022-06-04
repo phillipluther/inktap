31dd62a40566752a6323e40d6b2e5ed88efee126
Phillip Luther <dev@phillipluther.com>
2022-06-04 07:19:02 -0700

    Editor exploration

--- packages/inktap-app/src/App.tsx
+++ packages/inktap-app/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Editor from './components/editor';
 
+import './app.css';
+
 const App = () => {
-  return <Editor />;
+  return (
+    <>
+      <Editor />
+    </>
+  );
 };
 
 export default App;
